e5b7e3b1d759fc1844587503a5b1678cda321f57
Nguyen Minh Tuan <tnowad@gmail.com>
2024-07-27 22:13:39 +0700

    feat: initial project with spring initializr

---
+++ src/main/java/com/infinitynet/server/InfinityNetServerApplication.java
@@ -0,0 +1,13 @@
+package com.infinitynet.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityNetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityNetServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinitynet/server/InfinityNetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitynet.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityNetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
